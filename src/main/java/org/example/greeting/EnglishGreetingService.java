6c5debad317afaa10b8f8711526ac965ea525968
RiteshDB <riteshdb123@gmail.com>
2023-08-07 22:09:40 +0530

    Started with first service in project.

---
+++ src/main/java/org/example/greeting/EnglishGreetingService.java
@@ -0,0 +1,11 @@
+package org.example.greeting;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnglishGreetingService implements GreetingService{
+    @Override
+    public void greet(String name) {
+        System.out.println("Hello "+ name);
+    }
+}
---
+++ src/main/java/org/example/greeting/GreetingService.java
@@ -0,0 +1,5 @@
+package org.example.greeting;
+
+public interface GreetingService {
+    public void greet(String name);
+}
