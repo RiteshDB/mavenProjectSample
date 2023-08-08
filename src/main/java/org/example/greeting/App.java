81710794336d669bce4f7c2b3e140f05db91bcac
RiteshDB <riteshdb123@gmail.com>
2023-08-08 19:44:51 +0530

    Completion on basic Spring project implementation

--- src/main/java/org/example/greeting/App.java
+++ src/main/java/org/example/greeting/App.java
@@ -8,9 +8,10 @@ public class App {
 
         ApplicationContext context = new AnnotationConfigApplicationContext("org.example.greeting");
         EnglishGreetingService greetingService = (EnglishGreetingService) context.getBean("englishGreetingService");
-
+        EnglishGreetingService greetingService1 = (EnglishGreetingService) context.getBean("englishGreetingService");
         greetingService.greet("Ritesh");
         System.out.println("the time of this log is "+ greetingService.getTimeService().getTime());
+        System.out.println("Comparision of beans. "+(greetingService==greetingService1));
 
 
     }
